f8a139d7fcd8e24989e4458e4946377ad5bb9d8f
Oliver Futureworkz <oliver@futureworkz.com>
2016-01-16 14:49:48 +0700

    Add #say_something for father model

--- spec/models/father_spec.rb
+++ spec/models/father_spec.rb
@@ -8,4 +8,13 @@ describe Father, type: :model do
   context 'validations' do
     it { is_expected.to enumerize(:gender).in(:male) }
   end
+
+  describe '#say_something' do
+    let!(:bo)     { create :father, first_name: 'Bo' }
+    let!(:haha)   { create(:person, first_name: 'HaHa', father: bo) }
+
+    it 'show say_something' do
+      expect(haha.father.say_something).to eq "Hello, I am your father."
+    end
+  end
 end
